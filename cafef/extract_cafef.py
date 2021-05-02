58accee6fb8c0ebe6e5f3810b63fb34e5d2b0737
tranlysfw <tranlysfw@gmail.com>
2021-05-02 12:53:45 +0700

    cafef send key to end of html

--- cafef/extract_cafef.py
+++ cafef/extract_cafef.py
@@ -1,4 +1,5 @@
 import requests
+import time
 import re
 from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
@@ -9,56 +10,74 @@ import selenium.webdriver.support.ui as ui
 from database.tables import *
 
 
-def cafef_get_list_articles_main_page(number_of_loading=5):
+def cafef_get_list_articles_main_page(number_of_loading=10):
     """
     Retrieve all links of articles on main page of tinhte
 
     :number_of_loading: how many times we push the button
     :return: list of articles
     """
+    # SCROLL_PAUSE_TIME = 0.5
+    # # Get scroll height
+    # last_height = driver.execute_script("return document.body.scrollHeight")
+    # while True:
+    #     # Scroll down to bottom
+    #     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    #     # Wait to load page
+    #     time.sleep(SCROLL_PAUSE_TIME)
+    #     # Calculate new scroll height and compare with last scroll height
+    #     new_height = driver.execute_script("return document.body.scrollHeight")
+    #     if new_height == last_height:
+    #         break
+    #     last_height = new_height
+
+    main_page_url = "https://cafef.vn/tai-chinh-ngan-hang.chn"
+
     try:
         list_articles = []  # set output
         driver = webdriver.Firefox()  # using selenium with Firefox
-        driver.get(main_page_url)  # get main page of Tinhte
+        driver.get(main_page_url)  # get main page of cafef
     except Exception as e:
         print(f"[Get page] Cannot get content of page!, error code {e}")
         return None
 
     try:
-        button_more = driver.find_element_by_xpath("//button[contains(@class, 'load-more-btn')]")  # search button to load more
-
-        if not button_more:  # reserved
-            print(f"There is only one page!")
-        else:
-            for i in range(0, number_of_loading):  # count action of clicking
-                print(f"Clicked: {i + 1} times")
-                try:
-                    button_more.click()  # simulated clicking
-                    wait = ui.WebDriverWait(driver, timeout=20)  # wait for browser to load
-                    wait.until(lambda driver: driver.find_element_by_xpath("//button[contains(@class, 'load-more-btn')]"))
-                except Exception as e:
-                    print(f"[Load more articles] Error code {e}")
-                    continue
+        for j in range(0, number_of_loading):
+            button_more = ""
+            html = driver.find_element_by_tag_name('html')
+            for i in range(0, 100):
+                # html = driver.find_element_by_tag_name('html')
+                html.send_keys(Keys.END)
+                time.sleep(2)  # wait 2 seconds
+                button_more = driver.find_element_by_xpath("//a[contains(@class, 'sprite')]")  # search button to load more
+                if button_more:
+                    break
+            try:
+                button_more.click()  # simulated clicking
+                print(f"Clicked: {j + 1} times")
+            except Exception as e:
+                print(f"[Load more articles] {e}")
+                continue
 
     except Exception as e:
         print(f"[Handle click] Error code {e}")
 
     try:
-        articles = driver.find_elements_by_tag_name("article")  # find articles
+        articles = driver.find_elements_by_xpath("//li[contains(@class, 'tlitem')]//h3//a")  # find articles
         if not articles:
             print("None")
 
         for article in articles:
-            a_tag = article.find_elements_by_tag_name("a")
-
-            if a_tag:
-                # print(a_tag[0].get_attribute("href"))
-                list_articles.append(a_tag[0].get_attribute("href"))
+            print(f"article: {article.get_attribute('title')}")
+            # list_articles.append(a_tag[0].get_attribute("href"))
 
         articles.clear()
         driver.close()
-        print(f"The number of articles are collected: {len(list_articles)}")
+        # print(f"The number of articles are collected: {len(list_articles)}")
         return list_articles
 
     except Exception as e:
         print(f"[Get articles] Error code {e}")
+
+if (__name__ == "__main__"):
+    cafef_get_list_articles_main_page()
